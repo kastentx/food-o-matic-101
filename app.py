a6cf35234ccdb4661ebd01bddbd07defe5c4d372
Nick Kasten <kastentx@gmail.com>
2018-04-23 02:29:31 -0500

    major code refactor

--- app.py
+++ app.py
@@ -22,69 +22,55 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import logging
-import random
 import time
+import numpy as np
+import tensorflow as tf
+import requests
 
 from flask import Flask, jsonify, request
 
-import numpy as np
-import tensorflow as tf
-
-import requests
-
 app = Flask(__name__)
 
-def load_graph(model_file):
-  graph = tf.Graph()
-  graph_def = tf.GraphDef()
+def load_graph(path_to_model):
+    tf_graph = tf.Graph()
+    graph_def = tf.GraphDef()
+    with open(path_to_model, "rb") as model_file:
+        graph_def.ParseFromString(model_file.read())
+    with tf_graph.as_default():
+        tf.import_graph_def(graph_def)
+    return tf_graph
 
-  with open(model_file, "rb") as f:
-    graph_def.ParseFromString(f.read())
-  with graph.as_default():
-    tf.import_graph_def(graph_def)
+def read_tensor_from_image_url(image_data, image_url, height=224, width=224, mean=128, std=128):
+    if image_url.endswith(".png"):
+        image_reader = tf.image.decode_png(image_data, channels=3, name='png_reader')
+    elif image_url.endswith(".gif"):
+        image_reader = tf.squeeze(tf.image.decode_gif(image_data, name='gif_reader'))
+    elif image_url.endswith(".bmp"):
+        image_reader = tf.image.decode_bmp(image_data, name='bmp_reader')
+    else:
+        image_reader = tf.image.decode_jpeg(image_data, channels=3, name='jpeg_reader')
+    float_caster = tf.cast(image_reader, tf.float32)
+    dims_expander = tf.expand_dims(float_caster, 0)
+    resized = tf.image.resize_bilinear(dims_expander, [height, width])
+    normalized = tf.divide(tf.subtract(resized, [mean]), [std])
+    sess = tf.Session()
+    result = sess.run(normalized)
+    return result
 
-  return graph
+def load_labels(path_to_labels):
+    labels = []
+    proto_as_ascii_lines = tf.gfile.GFile(path_to_labels).readlines()
+    for label in proto_as_ascii_lines:
+        labels.append(label.rstrip())
+    return labels
 
-def read_tensor_from_image_url(image_data, image_url, input_height=224, input_width=224,
-				input_mean=128, input_std=128):
-  input_name = "file_reader"
-  output_name = "normalized"
-  if image_url.endswith(".png"):
-    image_reader = tf.image.decode_png(image_data, channels = 3,
-                                       name='png_reader')
-  elif image_url.endswith(".gif"):
-    image_reader = tf.squeeze(tf.image.decode_gif(image_data,
-                                                  name='gif_reader'))
-  elif image_url.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(image_data, name='bmp_reader')
-  else:
-    image_reader = tf.image.decode_jpeg(image_data, channels = 3,
-                                        name='jpeg_reader')
-  float_caster = tf.cast(image_reader, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0);
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
-  result = sess.run(normalized)
-
-  return result
-
-def load_labels(label_file):
-  label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-  for l in proto_as_ascii_lines:
-    label.append(l.rstrip())
-  return label
-
-def get_ranking(top_5_labels_with_scores):
-  x = top_5_labels_with_scores
-  sorted_x = sorted(x, key=x.get, reverse=True)
-  return {rank: key for rank, key in enumerate(sorted_x, 1)}
+def get_ranking(top_5_labels_scores):
+    sorted_labels = sorted(top_5_labels_scores, key=top_5_labels_scores.get, reverse=True)
+    return {rank: key for rank, key in enumerate(sorted_labels, 1)}
 
 @app.route('/')
 def test_route():
-  return jsonify("Hello Flask.")
+    return jsonify("Hello Flask.")
 
 @app.route('/predict_web')
 def classify_web():
@@ -92,50 +78,28 @@ def classify_web():
     image_url = request.args['url']
     image_data = requests.get(image_url, stream=True).content
     image_tensor = read_tensor_from_image_url(image_data, image_url)
-
     # make prediction
-    with tf.Session(graph=graph) as sess:
+    loaded_graph = load_graph("retrained_graph.pb")
+    with tf.Session(graph=loaded_graph) as sess:
+        input_layer = "Placeholder"
+        output_layer = "final_result"
+        input_name = "import/" + input_layer
+        output_name = "import/" + output_layer
+        input_operation = loaded_graph.get_operation_by_name(input_name)
+        output_operation = loaded_graph.get_operation_by_name(output_name)
         start = time.time()
-        results = sess.run(output_operation.outputs[0],
-                      {input_operation.outputs[0]: image_tensor})
-        end=time.time()
+        results = sess.run(output_operation.outputs[0], {input_operation.outputs[0]: image_tensor})
+        end = time.time()
         results = np.squeeze(results)
-
         top_k = results.argsort()[-5:][::-1]
-        labels = load_labels(label_file)
-
+        labels = load_labels("retrained_labels.txt")
     print('\nEvaluation time (1-image): {:.3f}s\n'.format(end-start))
-
-    top_5_rank = {}
     top_5_score = {}
     for i in top_k:
         print(labels[i], results[i])
         top_5_score[labels[i]] = np.float64(results[i]).item()
-        top_5_rank = get_ranking(top_5_score)
-
-
-    return jsonify(rank=top_5_rank, score=top_5_score)
-
-# GOTTA HAVE THOSE GLOBALS DEFINED YALL
-
-# TensorFlow configuration/initialization
-model_file = "retrained_graph.pb"
-label_file = "retrained_labels.txt"
-input_height = 224
-input_width = 224
-input_mean = 128
-input_std = 128
-input_layer = "Placeholder"
-output_layer = "final_result"
-
-# Load TensorFlow Graph from disk
-graph = load_graph(model_file)
-
-# Grab the Input/Output operations
-input_name = "import/" + input_layer
-output_name = "import/" + output_layer
-input_operation = graph.get_operation_by_name(input_name);
-output_operation = graph.get_operation_by_name(output_name);
+    return jsonify(rank=get_ranking(top_5_score), score=top_5_score, \
+                   eval_time='{:.3f}s'.format(end-start))
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
