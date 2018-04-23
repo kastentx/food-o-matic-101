dbba5825bf9e66235b3e8b17af5d71753fb4b337
Nick Kasten <kastentx@gmail.com>
2018-04-22 23:48:19 -0500

    ts the __main__ graph undefined issue

--- app.py
+++ app.py
@@ -113,6 +113,32 @@ def test_route():
 
 @app.route('/predict_web')
 def classify_web():
+
+    # putting this in here so graph isn't undefined
+    # TensorFlow configuration/initialization
+    model_file = "retrained_graph.pb"
+    label_file = "retrained_labels.txt"
+    input_height = 224
+    input_width = 224
+    input_mean = 128
+    input_std = 128
+    input_layer = "Placeholder"
+    output_layer = "final_result"
+
+    # Load TensorFlow Graph from disk
+    graph = load_graph(model_file)
+
+    # Grab the Input/Output operations
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+    input_operation = graph.get_operation_by_name(input_name);
+    output_operation = graph.get_operation_by_name(output_name);
+    # the sutff above was taken from main
+
+
+
+
+
     # load image from url
     image_url = request.args['url']
     #image_data = Image.open(requests.get(image_url, stream=True).raw)
@@ -198,4 +224,4 @@ if __name__ == '__main__':
 
     # Initialize the Flask Service
     # Obviously, disable Debug in actual Production
-    app.run(debug=True, port=8000)
+    app.run()
